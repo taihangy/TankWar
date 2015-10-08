9c98fc7c965790e2288b4b4c37b1fc04d6cadb2f
taihangy <yetaihang.zju@gmail.com>
2015-10-07 20:00:10 -0400

    TankClient

---
+++ src/TankClient.java
@@ -0,0 +1,17 @@
+/**
+ * @Title: TankClient.java
+ * @Package 
+ * @Description: TODO(添加描述)
+ * @author A18ccms A18ccms_gmail_com
+ * @date Oct 7, 2015 7:59:43 PM
+ * @version V1.0
+ */
+public class TankClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
