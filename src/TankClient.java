57aa9cf35adc96514cf974e32f3aa654d9e75794
taihangy <yetaihang.zju@gmail.com>
2015-10-07 20:10:45 -0400

    Launch Frame and closing event

--- src/TankClient.java
+++ src/TankClient.java
@@ -6,11 +6,28 @@
  * @date Oct 7, 2015 7:59:43 PM
  * @version V1.0
  */
-public class TankClient {
 
+import java.awt.*;
+import java.awt.event.*;
+public class TankClient extends Frame {
+	
+	public void launchFram() {
+		this.setLocation(400, 300);
+		this.setSize(800, 600);
+		this.setTitle("TankWar");
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e) {
+				super.windowClosing(e);
+				System.exit(0);
+			}
+		});
+		this.setResizable(false);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TankClient tc = new TankClient();
+		tc.launchFram();
 	}
 
 }
